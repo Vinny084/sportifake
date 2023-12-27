0729a1b43bf9b74f0e4a176d3532ecf98adcebe4
Vinny084 <josevinicius195@gmail.com>
2023-12-27 11:46:29 -0300

    1

--- api/models.py
+++ api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class User(models.Model):
     nome = models.CharField(max_length=100)
-    usernome = models.CharField(max_length=100)
+    username = models.CharField(max_length=100)
     email = models.EmailField()
     address = models.CharField(max_length=20)
     phone = models.CharField(max_length=100)
